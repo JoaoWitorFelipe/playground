d7fe14d1c28c97ae8947558579d86ba0b3c920c3
Joao Witor <42946342+JoaoWitorFelipe@users.noreply.github.com>
2021-12-06 14:20:22 -0300

    clear dart files

--- dart/lib/todo_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-class Todo extends Equatable {
-  final String text;
-  Todo({required this.text});
-
-  @override
-  List<Object> get props => [text];
-}
--- dart/test/main_test.dart
+++ dart/test/main_test.dart
@@ -1,22 +1,5 @@
-import 'package:dart/bloc.dart';
-import 'package:dart/todo_model.dart';
-import 'package:dart/todo_state.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final MainBloc mainBloc = MainBloc();
-
-  test('should fetch todo', () {
-    final text = 'hello';
-    final data = Todo(text: text);
-
-    expectLater(
-      mainBloc.stream,
-      emitsInOrder(
-        [LoadingState(), ResponseState<Todo>(data: data)],
-      ),
-    );
-
-    mainBloc.sink.add(text);
-  });
+  test('it', () {});
 }
